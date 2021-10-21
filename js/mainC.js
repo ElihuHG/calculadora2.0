3338c3ec139dd5193c8778a1e603cff6bb918446
ElihuHG <madafakanew@hotmail.com>
2021-10-21 15:33:57 -0500

    final

--- js/mainC.js
+++ js/mainC.js
@@ -1,39 +1,39 @@
 
 
-
 $(document).ready(function(){
-    $("#btnCalcular").click(function(){
-       var operador = $("#operador").val();
-       var v1 = parseInt( $("#primNum").val());
-       var v2 = parseInt( $("#segNum").val());
-       var res=''; 
+  $("#btnCalcular").click(function(){
+     var operador = $("#operador").val();
+     var v1 = parseInt( $("#primNum").val());
+     var v2 = parseInt( $("#segNum").val());
+     var res=''; 
 
 
 
-       if(operador=='+'){
-         res = v1 + v2;
-        
-       } 
-       else if(operador == '-'){
-           res = v1-v2;
-           
-         }  
-       
-       else if(operador == '*'){
-        res = v1*v2;
-        
-         }  
+     if(operador=='+'){
+       res = v1 + v2;
 
-        else if(operador == '/'){
-        res = v1/v2;
-       
-         }  
+     } 
+     else if(operador == '-'){
+         res = v1-v2;
 
-         $("#resultado").text("el resultado es " + res);
+       }  
 
-     
-        
-    });
-  
+     else if(operador == '*'){
+      res = v1*v2;
+
+       }  
+
+      else if(operador == '/'){
+      res = v1/v2;
+
+       }  
+
+       $("#resultado").text("el resultado es " + res);
+
+
+
+  });
+
 });
+
 
